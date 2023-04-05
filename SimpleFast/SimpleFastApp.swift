d339f9960e6a56a493182b57038c11f33e961255
Biz <bizkarimi@hotmail.com>
2023-04-05 01:45:22 -0700

    Initial Commit

---
+++ SimpleFast/SimpleFastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFastApp.swift
+//  SimpleFast
+//
+//  Created by Biz Karimi on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
